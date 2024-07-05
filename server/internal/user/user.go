e53111850577be90ad94ea25d132e2dbeacc1863
Junior <eecfredes@gmail.com>
2024-07-04 21:03:36 -0300

    :rocket: Creating the user req and res struct

--- server/internal/user/user.go
+++ server/internal/user/user.go
@@ -13,3 +13,19 @@ type Repository interface {
 	CreateUser(ctx context.Context, user *User) (*User, error)
 	// GetUserByEmail(ctx context.Context, email string) (*User, error)
 }
+
+type CreateUserReq struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CreateUserRes struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+type Service interface {
+	CreateUser(c context.Context, req *CreateUserReq) (*CreateUserReq, error)
+}
