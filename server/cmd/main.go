c24cd58b885143b23970895e31e3eec6ab60b72f
Junior <eecfredes@gmail.com>
2024-07-05 22:18:11 -0300

    :rocket: Connecting createUser repository to database

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"log"
 	"server/db"
+	"server/internal/user"
 )
 
 func main() {
-	_, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
 	if err != nil {
 		log.Fatalf("could not initialize databese connection: %s", err)
 	}
+
+	userRap := user.NewRepository(dbConn.GetDB())
+	userSvc := user.NewService(userRap)
+	userHandler := user.NewHandler(userSvc)
 
 }
