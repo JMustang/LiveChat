fe7e669e0c4815246c9f34d0d7a50d119625c472
Junior <eecfredes@gmail.com>
2024-07-07 12:18:50 -0300

    :rocket: LoginUserReq and LoginUserRes

--- server/internal/user/user.go
+++ server/internal/user/user.go
@@ -9,11 +9,6 @@ type User struct {
 	Password string `json:"password"`
 }
 
-type Repository interface {
-	CreateUser(ctx context.Context, user *User) (*User, error)
-	// GetUserByEmail(ctx context.Context, email string) (*User, error)
-}
-
 type CreateUserReq struct {
 	Username string `json:"username"`
 	Email    string `json:"email"`
@@ -26,6 +21,23 @@ type CreateUserRes struct {
 	Email    string `json:"email"`
 }
 
+type LoginUserReq struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type LoginUserRes struct {
+	accessToken string
+	ID          string `json:"id"`
+	Username    string `json:"username"`
+}
+
+type Repository interface {
+	CreateUser(ctx context.Context, user *User) (*User, error)
+	GetUserByEmail(ctx context.Context, email string) (*User, error)
+}
+
 type Service interface {
 	CreateUser(c context.Context, req *CreateUserReq) (*CreateUserRes, error)
+	Login(c context.Context, req *LoginUserReq) (*LoginUserRes, error)
 }
