1b9811a425b1ad84baee9d037b44db3a71c79119
Junior <eecfredes@gmail.com>
2024-07-12 21:42:39 -0300

    :rocket: read message function

--- server/internal/ws/client.go
+++ server/internal/ws/client.go
@@ -1,6 +1,8 @@
 package ws
 
 import (
+	"log"
+
 	"github.com/gorilla/websocket"
 )
 
@@ -29,5 +31,30 @@ func (c *Client) writeMessage() {
 		}
 
 		c.Conn.WriteJSON(message)
+	}
+}
+
+func (c *Client) readMessage(hub *Hub) {
+	defer func() {
+		hub.Unregister <- c
+		c.Conn.Close()
+	}()
+
+	for {
+		_, m, err := c.Conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			break
+		}
+
+		msg := &Message{
+			Content:  string(m),
+			RoomID:   c.RoomID,
+			Username: c.Username,
+		}
+
+		hub.Broadcast <- msg
 	}
 }
